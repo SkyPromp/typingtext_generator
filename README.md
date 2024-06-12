0eaac1e63f4bc795cc825df3aee2121764445dc3
Max Poppe <max.poppe@ugent.be>
2024-06-12 16:48:56 +0200

    make more interactable with cli by giving parameters

--- README.md
+++ README.md
@@ -6,6 +6,33 @@ The words in the file, e.g. words.txt should be separated by newlines
 ## Compile
 gcc -o run.exe filename
 ## words_numbers.c
-run.exe -> this will generate the data using the words in words.txt
-run.exe filename -> this will generate the data using the words in the filename
+examples  
+  
+run.exe
+- File: words.txt
+- Minimum number length: 2
+- Maximum number length: 5
+- Numbers to words ratio: 0.3
+- Amount of words + numbers: 25
+
+run.exe data.txt
+- File: data.txt
+- Minimum number length: 2
+- Maximum number length: 5
+- Numbers to words ratio: 0.3
+- Amount of words + numbers: 25
+
+run.exe 1 3 0.5 10
+- File: words.txt
+- Minimum number length: 1
+- Maximum number length: 3
+- Numbers to words ratio: 0.5
+- Amount of words + numbers: 10
+
+run.exe data.txt 1 3 0.5 10
+- File: data.txt
+- Minimum number length: 1
+- Maximum number length: 3
+- Numbers to words ratio: 0.5
+- Amount of words + numbers: 10
 
--- words_numbers.c
+++ words_numbers.c
@@ -2,31 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#define MIN_NUMBER_LENGTH 2
-#define MAX_NUMBER_LENGTH 5
-#define NUMBERS_RATIO 0.3
-#define TOKENS 25
 
 typedef enum text_type{
 	WORD,
 	NUMBER
 } text_type;
 
+typedef struct format_data{
+	char FILE_NAME[255];
+	int MIN_NUMBER_LENGTH;
+	int MAX_NUMBER_LENGTH;
+	float NUMBERS_RATIO;
+	int TOKENS;
+} format_data;
+
+void logData(format_data*);
 size_t getFileLines(char*, size_t*);
-int getNumber();
+int getNumber(int, int);
 char *getWord();
-text_type next();
-void outputData(size_t max_word_length, char [][max_word_length], size_t);
+text_type next(float);
+void outputData(size_t max_word_length, char [][max_word_length], size_t, format_data*);
 
 
-int main(){
+int main(int argc, char *argv[]){
+	format_data *data;
+	data = (format_data *)malloc(sizeof(format_data));
+	
+	strcpy(data->FILE_NAME, "words.txt");
+	data->MIN_NUMBER_LENGTH = 2;
+	data->MAX_NUMBER_LENGTH = 5;
+	data->NUMBERS_RATIO = 0.3;
+	data->TOKENS = 25;
+
+	int TOKENS = 25;
+	if(argc == 2) strcpy(data->FILE_NAME, argv[1]);
+	if(argc == 5){
+		data->MIN_NUMBER_LENGTH = atoi(argv[1]);
+		data->MAX_NUMBER_LENGTH = atoi(argv[2]);
+		data->NUMBERS_RATIO = atof(argv[3]);
+		data->TOKENS = atoi(argv[4]);
+	}
+	if(argc == 6){
+		strcpy(data->FILE_NAME, argv[1]);
+		data->MIN_NUMBER_LENGTH = atoi(argv[2]);
+		data->MAX_NUMBER_LENGTH = atoi(argv[3]);
+		data->NUMBERS_RATIO = atof(argv[4]);
+		data->TOKENS = atoi(argv[5]);
+	}
+
 	srand(time(NULL));
-	char *file_name = "words.txt";
 	size_t MAX_WORD_LENGTH = 0;
-	const size_t FILE_LINES = getFileLines(file_name, &MAX_WORD_LENGTH);
+	const size_t FILE_LINES = getFileLines(data->FILE_NAME, &MAX_WORD_LENGTH);
 	MAX_WORD_LENGTH++;
 
-	FILE *fptr = fopen(file_name, "r");
+	FILE *fptr = fopen(data->FILE_NAME, "r");
 	char words[FILE_LINES][MAX_WORD_LENGTH];
 	char word[MAX_WORD_LENGTH];
 	size_t index = 0;
@@ -38,16 +67,24 @@ int main(){
 		strcpy(words[index++], word);
 	}
 
-	for(int i = 0; i < TOKENS - 1; i++) {
-		outputData(MAX_WORD_LENGTH, words, FILE_LINES);
+	for(int i = 0; i < data->TOKENS - 1; i++) {
+		outputData(MAX_WORD_LENGTH, words, FILE_LINES, data);
 		putchar(' ');
 	}
 
-	outputData(MAX_WORD_LENGTH, words, FILE_LINES);
+	outputData(MAX_WORD_LENGTH, words, FILE_LINES, data);
 
 	fclose(fptr);
 
 	return 0;
+}
+
+void logData(format_data *data){
+	printf("\n%s\n", data->FILE_NAME);
+	printf("%d\n", data->MIN_NUMBER_LENGTH);
+	printf("%d\n", data->MAX_NUMBER_LENGTH);
+	printf("%f\n", data->NUMBERS_RATIO);
+	printf("%d\n", data->TOKENS);
 }
 
 size_t getFileLines(char* file_name, size_t *max_word_length){
@@ -71,7 +108,7 @@ size_t getFileLines(char* file_name, size_t *max_word_length){
 	return index;
 }
 
-int getNumber(){
+int getNumber(int MIN_NUMBER_LENGTH, int MAX_NUMBER_LENGTH){
 	const int digits = rand() % ((MAX_NUMBER_LENGTH + 1 - MIN_NUMBER_LENGTH)) + MIN_NUMBER_LENGTH;
 
 	if (digits == 1) return (rand() % 10);
@@ -91,7 +128,7 @@ char *getWord(){
 	return "hoi";
 }
 
-text_type next(){
+text_type next(float NUMBERS_RATIO){
 	const int percent = RAND_MAX * NUMBERS_RATIO;
 	int value = rand();
 
@@ -100,8 +137,8 @@ text_type next(){
 	return NUMBER;
 }
 
-void outputData(size_t max_word_length, char words[][max_word_length], size_t words_amount){
-	int token = next();
+void outputData(size_t max_word_length, char words[][max_word_length], size_t words_amount, format_data *data){
+	int token = next(data->NUMBERS_RATIO);
 
 	switch (token){
 		case WORD:
@@ -109,7 +146,7 @@ void outputData(size_t max_word_length, char words[][max_word_length], size_t wo
 			printf("%s", words[rand() % words_amount]);
 			break;
 		case NUMBER:
-			printf("%d", getNumber());
+			printf("%d", getNumber(data->MIN_NUMBER_LENGTH, data->MAX_NUMBER_LENGTH));
 			break;
 		default:
 			printf("Unexpected type token: %d", token);
